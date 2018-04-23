59ef0c8bc0ece4e92e0a8fe6b02a37952a8ee491
sugata <sugatabhar@yahoo.com>
2018-04-23 16:29:01 +0530

    added CURLOPT_INFILESIZE for content-length

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 $ch = curl_init();
+$file_path_str = 'test.pdf';
+$fh_res = fopen($file_path_str, 'r');
 
 curl_setopt($ch, CURLOPT_URL, "http://pdfx.cs.man.ac.uk");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -8,6 +10,9 @@ $post = array(
 );
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_POST, 1);
+
+curl_setopt($ch, CURLOPT_INFILE, $fh_res);
+curl_setopt($ch, CURLOPT_INFILESIZE, filesize($file_path_str));
 
 $headers = array();
 $headers[] = "Content-Type: application/pdf";
