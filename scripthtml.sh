9c73a4b1a748aac04e87564d7098f142c18a42e3
sugata <sugatabhar@yahoo.com>
2018-04-25 10:41:04 +0530

    Committing script html shell script

---
+++ scripthtml.sh
@@ -0,0 +1,6 @@
+find uploads/ -name "*.pdf" | while read file;
+do
+curl --data-binary @"$file" -H "Content-Type: application/pdf" -L "http://pdfx.cs.man.ac.uk" > "${file}x.html";
+done
+zip -r output.zip uploads
+chmod 777 output.zip
