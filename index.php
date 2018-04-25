e1efc95e548fb738c6145a93122c35725878d241
sugata <sugatabhar@yahoo.com>
2018-04-25 10:52:35 +0530

    added isset

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ if (!file_exists('uploads')) {
 	mkdir('uploads', 0700, true);
 }
 if(move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name'])) {
-	if($_POST("outputchoice") == "html") {
+	if(isset($_POST["outputchoice"])&&$_POST("outputchoice") == "html") {
+		echo "HTML output is selected";
 		chmod("scripthtml.sh",0700);
 		shell_exec('./scripthtml.sh');
 	}elseif($_POST("outputchoice") == "xml") {{
