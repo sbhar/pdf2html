dabec9f44595e7ce238893150e1bb58d2e960a3b
sugata <sugatabhar@yahoo.com>
2018-04-25 11:11:01 +0530

    debug

--- index.php
+++ index.php
@@ -5,7 +5,18 @@ if (!file_exists('uploads')) {
 }
 $outputchoice = htmlentities($_POST['outputchoice'], ENT_QUOTES, "UTF-8");
 echo $outputchoice;
-exit;
+if(move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name'])) {
+	if(isset($outputchoice == "html") {
+		echo "HTML output is selected";
+		chmod("scripthtml.sh",0700);
+		shell_exec('./scripthtml.sh');
+	}elseif($outputchoice == "xml") {
+		chmod("script.sh",0700);
+		shell_exec('./script.sh');
+	}
+}else{
+	echo "There was an error uploading the file, please try again!";
+}
 
 
 ?>
