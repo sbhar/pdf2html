8379d79daf971ea908a1da1b82e0c503c8ea54c8
sugata <sugatabhar@yahoo.com>
2018-04-25 11:01:57 +0530

    debug

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ header("Access-Control-Allow-Origin: *");
 if (!file_exists('uploads')) {
 	mkdir('uploads', 0700, true);
 }
+$outputchoice = htmlentities($_POST['outputchoice'], ENT_QUOTES, "UTF-8");
 if(move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name'])) {
-	if(isset($_POST["outputchoice"])&&$_POST("outputchoice") == "html") {
+	if(isset($outputchoice == "html") {
 		echo "HTML output is selected";
 		chmod("scripthtml.sh",0700);
 		shell_exec('./scripthtml.sh');
-	}elseif($_POST("outputchoice") == "xml") {{
+	}elseif($outputchoice == "xml") {
 		chmod("script.sh",0700);
 		shell_exec('./script.sh');
 	}
