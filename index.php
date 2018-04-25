63c867b34862b47eb3139b0130f1f7c1aa6c4ddf
sugata <sugatabhar@yahoo.com>
2018-04-25 10:35:41 +0530

    Added the option of html

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ if (!file_exists('uploads')) {
 	mkdir('uploads', 0700, true);
 }
 if(move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name'])) {
-	chmod("script.sh",0700);
-	shell_exec('./script.sh');
+	if($_POST(outputchoice) === "xml") {
+		chmod("script.sh",0700);
+		shell_exec('./script.sh');
+	}else{
+		chmod("scripthtml.sh",0700);
+		shell_exec('./scripthtml.sh');
+	}
 }else{
 	echo "There was an error uploading the file, please try again!";
 }
